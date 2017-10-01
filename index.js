5a9fad4d44c56f53fa00bb2bc4bc1d111269d19b
Mark Lee <mark@protopie.io>
2017-10-01 12:23:49 +0900

    12시23분

---
+++ index.js
@@ -0,0 +1,37 @@
+const {app, BrowserWindow} = require('electron');
+
+app.on('ready', () => {
+    console.log('ready');
+
+    console.log(app.getAppPath());
+    console.log(app.getPath('home'));
+    console.log(app.getPath('userData'));
+    console.log(app.getPath('temp'));
+    
+    new BrowserWindow();
+});
+
+app.on('window-all-closed', () => {
+    console.log('window-all-closed');
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('before-quit', (event) => {
+    console.log('before-quit');
+    // event.preventDefault();
+});
+
+app.on('will-quit', (event) => {
+    console.log('will-quit');
+    // event.preventDefault();
+});
+
+app.on('quit', () => {
+    console.log('quit');
+});
+
+app.on('activate', (event, hasVisibleWindows) => {
+    console.log('activate', hasVisibleWindows);
+});
